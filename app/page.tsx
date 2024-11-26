290717dd196b9e6d2782594c098750199810f2e0
Aphiwatch <Aphiwatch09@gmail.com>
2024-11-26 22:53:22 +0700

    work exp

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import Link from "next/link";
 import { FaLinkedinIn } from "react-icons/fa";
-import { TbBrandGithubFilled, TbDownload  } from "react-icons/tb";
+import { TbBrandGithubFilled, TbDownload } from "react-icons/tb";
 import { FcContacts } from "react-icons/fc";
 import Image from "next/image";
 import profilePic from "@/assets/image/01.png";
 import { FaLocationDot } from "react-icons/fa6";
 
+const workexp = [
+  {
+    role: "Software Developer",
+    company: "Extend It Resource Co., Ltd.",
+    date: "Jul - Oct 2024",
+  },
+  {
+    role: "Mobile Developer",
+    company: "KTW",
+    date: "May 2024",
+  }
+];
+ 
 export default function Home() {
   return (
     <div className="flex flex-auto justify-center items-center xl:px-12 font-[family-name:var(--font-geist-sans)]">
@@ -27,7 +40,9 @@ export default function Home() {
           <div className="flex justify-center items-center">
             <div className="flex flex-col justify-center items-start">
               <p className="text-5xl font-bold pb-2">Hello I'm Bank</p>
-              <h1 className="text-4xl font-regular pb-2">Aphiwat Chalongtham</h1>
+              <h1 className="text-4xl font-regular pb-2">
+                Aphiwat Chalongtham
+              </h1>
               <div className="flex justify-center items-center gap-2 pb-2">
                 <FaLocationDot />
                 <p className="">Surin, Thailand</p>
@@ -37,16 +52,16 @@ export default function Home() {
 
           {/* Button */}
           <div className="flex justify-center p-8 items-center">
-            <button className="
+            <button
+              className="
               bg-primary text-primary-foreground hover:bg-primary/90 
-              rounded-xl font-medium text-2xl">
+              rounded-xl font-medium text-2xl"
+            >
               <div className="p-6 flex justify-center items-center gap-2">
                 <p>Download CV</p>
                 <TbDownload />
               </div>
-              
             </button>
-            
           </div>
         </div>
 
@@ -95,7 +110,18 @@ export default function Home() {
 
         {/* about section */}
         <div className="col-span-3 row-span-4 bg-accent rounded-3xl flex justify-center items-center shadow-lg">
-          About(work exp.)
+            <div className="flex flex-col justify-center gap-16">
+              {workexp.map((exp) => (
+                <div className="text-2xl">
+                  <p className="text-primary">{exp.role}</p>
+                  <p className="font-bold">{exp.company}</p>
+                  <div className="flex items-center gap-3">
+                    <span className="w-[8px] h-[8px] rounded-full bg-primary"></span>
+                    <p>{exp.date}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
         </div>
 
         {/* contact section */}
